d7910d19bb16a3371bf0e487f5db4985ae77822e
GiovanniCalia <johnca@outlook.it>
2022-03-16 12:23:35 +0100

    Correzione problema js e crezione v-for per i contatti

--- js/script.js
+++ js/script.js
@@ -7,50 +7,57 @@ const app = new Vue({
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_1.jpg",
+                altImage: "avatar 1",
             },
             {
                 name: "Fabio",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_2.jpg",
+                altImage: "avatar 2",
             },
             {
                 name: "Samuele",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_3.jpg",
+                altImage: "avatar 3",
             },
             {
                 name: "Alessandro B.",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_4.jpg",
+                altImage: "avatar 4",
             },
             {
                 name: "Alessandro L.",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_5.jpg",
+                altImage: "avatar 5",
             },
             {
                 name: "Claudia",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_6.jpg",
+                altImage: "avatar 6",
             },
             {
                 name: "Federico",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_7.jpg",
+                altImage: "avatar 7",
             },
             {
                 name: "Davide",
                 lastMessage: "Ultimo messaggio inviato",
                 hour: "12:00",
                 image: "img/avatar_8.jpg",
+                altImage: "avatar 8",
             },
-
-        ]
-    }
+        ],
+    },
 })

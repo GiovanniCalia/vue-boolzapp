0cbd230d1ae69805b10c8a7a47c6082bf237d25c
GiovanniCalia <johnca@outlook.it>
2022-03-16 17:49:18 +0100

    Aggiunta orario

--- js/script.js
+++ js/script.js
@@ -11,68 +11,58 @@ const app = new Vue({
             {
                 name: "Michele",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "DateTime.fromISO('2017-W33-4')",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_1.jpg",
                 altImage: "avatar 1",
             },
             {
                 name: "Fabio",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_2.jpg",
                 altImage: "avatar 2",
             },
             {
                 name: "Samuele",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_3.jpg",
                 altImage: "avatar 3",
             },
             {
                 name: "Alessandro B.",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_4.jpg",
                 altImage: "avatar 4",
             },
             {
                 name: "Alessandro L.",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_5.jpg",
                 altImage: "avatar 5",
             },
             {
                 name: "Claudia",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_6.jpg",
                 altImage: "avatar 6",
             },
             {
                 name: "Federico",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_7.jpg",
                 altImage: "avatar 7",
             },
             {
                 name: "Davide",
                 lastMessage: "Ultimo messaggio inviato",
-                hour: "12:00",
+                hour: luxon.DateTime.now().toFormat("hh/mm"),
                 image: "img/avatar_8.jpg",
                 altImage: "avatar 8",
-            },
-        ],
-        messageUser: [
-            {
-                message: "Hai portato a spasso il cane?",
-                hour: "15.30",
-            },
-            {
-                message: "Ricordati di stendere i panni",
-                hour: "15:50",
             },
         ],
         textMessage: [
@@ -92,11 +82,11 @@ const app = new Vue({
     methods:{
         addMessage(){
             if (this.newMessage.message != ""){
+                let getTime = luxon.DateTime.now().toLocaleString();
+                console.log(getTime)
                 this.textMessage.push({...this.newMessage});
-                this.textMessage
                 this.newMessage.message = "";
             }
-        }
-        
-    }
+        }, 
+    },
 })

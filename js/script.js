5513fa7827b6496c98b811b97e30aefc5d2f51b1
GiovanniCalia <johnca@outlook.it>
2022-03-16 11:27:06 +0100

    Collegamento a Javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
 const app = new Vue({
-    el: "#root",
+    el: "#container",
+    data: {
+        contacts: [
+            {
+                name: "Michele",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_1.jpg",
+            },
+            {
+                name: "Fabio",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_2.jpg",
+            },
+            {
+                name: "Samuele",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_3.jpg",
+            },
+            {
+                name: "Alessandro B.",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_4.jpg",
+            },
+            {
+                name: "Alessandro L.",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_5.jpg",
+            },
+            {
+                name: "Claudia",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_6.jpg",
+            },
+            {
+                name: "Federico",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_7.jpg",
+            },
+            {
+                name: "Davide",
+                lastMessage: "Ultimo messaggio inviato",
+                hour: "12:00",
+                image: "img/avatar_8.jpg",
+            },
+        ],
+    }
 })
